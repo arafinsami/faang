1ff758be8fd3c944f55abff6d1579a821bf00013
MD. SAMIUL ARAFIN <sami.cse.1112@gmail.com>
2026-08-02 20:18:33 +0900

    updated _07_02_EquilibriumIndex

--- src/main/java/com/faang/array/basic/_07_02_EquilibriumIndex.java
+++ src/main/java/com/faang/array/basic/_07_02_EquilibriumIndex.java
@@ -8,12 +8,13 @@ public class _07_02_EquilibriumIndex {
 	}
 
 	private static int findEquilibrium(int[] arr) {
-		int[] prefix = new int[arr.length];
+		int length = arr.length;
+		int[] prefix = new int[length];
 		prefix[0] = arr[0];
-		for (int i = 1; i < arr.length; i++) {
+		for (int i = 1; i < length; i++) {
 			prefix[i] = prefix[i - 1] + arr[i];
 		}
-		int total = prefix[arr.length - 1];
+		int total = prefix[length - 1];
 		for (int i = 0; i < arr.length; i++) {
 			int leftSum = (i == 0) ? 0 : prefix[i - 1];
 			int rightSum = total - prefix[i];
