756e7c11b1fab6c0404ba31f588c2e03d82179bb
MD. SAMIUL ARAFIN <sami.cse.1112@gmail.com>
2026-09-22 02:17:33 +0900

    Min and max in window done

--- src/main/java/com/faang/array/sliding/fixed/_5_MinInWindow.java
+++ src/main/java/com/faang/array/sliding/fixed/_5_MinInWindow.java
@@ -1,6 +1,29 @@
 package com.faang.array.sliding.fixed;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
 public class _5_MinInWindow {
 	public static void main(String[] args) {
+		int[] array = { 1, 4, 2, 5 };
+		int length = array.length;
+		int windowSize = 2;
+		List<Integer> result = new ArrayList<Integer>();
+		Deque<Integer> deque = new ArrayDeque<Integer>();
+		for (int index = 0; index < length; index++) {
+			while (!deque.isEmpty() && deque.peekFirst() < index - windowSize + 1) {
+				deque.removeFirst();
+			}
+			while (!deque.isEmpty() && array[deque.peekLast()] > array[index]) {
+				deque.removeLast();
+			}
+			deque.addLast(index);
+			if (index >= windowSize - 1) {
+				result.add(array[deque.peekFirst()]);
+			}
+		}
+		System.out.println(result);
 	}
 }
